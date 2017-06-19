52bee75689209c8e9222c557598635165bb9985d
Johan Brenes <johanb2196@gmail.com>
2017-06-18 23:47:28 -0600

    CRT

--- FileSystem/src/Model/FileSystem.java
+++ FileSystem/src/Model/FileSystem.java
@@ -1,10 +1,23 @@
 package Model;
 
+import Commands.CHDIR;
+import Commands.CPY;
+import Commands.CRT;
+import Commands.FLE;
+import Commands.LDIR;
+import Commands.MFLE;
+import Commands.MKDIR;
+import Commands.MOV;
+import Commands.PPT;
+import Commands.REM;
+import Commands.TREE;
+import Commands.VIEW;
+
 public class FileSystem {
 	private String uID;
 	private FileComponent root;
 	private FileComponent currentFile;
-	private Disk disk;
+	public Disk disk;
 	private String userID;
 	
 	public FileSystem(String pUID){
@@ -15,9 +28,53 @@ public class FileSystem {
 		userID = "USERID01";*/
 	}
 	
-	public void execCommand(Command command, String[] args){
+	public void waitCommand(String command, String[]args){
+		switch(command.toUpperCase()){
+			case "CRT":
+				execCommand(Command.CRT, args);
+				break;
+			case "FLE":
+				execCommand(Command.FLE, args);
+				break;
+			case "MKDIR":
+				execCommand(Command.MKDIR, args);
+				break;
+			case "CHDIR":
+				execCommand(Command.CHDIR, args);
+				break;
+			case "LDIR":
+				execCommand(Command.LDIR, args);
+				break;
+			case "MFLE":
+				execCommand(Command.MFLE, args);
+				break;
+			case "PPT":
+				execCommand(Command.PPT, args);
+				break;
+			case "VIEW":
+				execCommand(Command.VIEW, args);
+				break;
+			case "CPY":
+				execCommand(Command.CPY, args);
+				break;
+			case "MOV":
+				execCommand(Command.MOV, args);
+				break;
+			case "REM":
+				execCommand(Command.REM, args);
+				break;
+			case "TREE":
+				execCommand(Command.TREE, args);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	public void execCommand(Command pCommand, String[] args){
 		CommandFactory cmdFactory = new CommandFactory();
-		cmdFactory.getCommand(command, args);
+		CommandAbstract com = cmdFactory.getCommand(pCommand, args);
+		com.exec(args);
 	}
 
 	public String getuID() {
